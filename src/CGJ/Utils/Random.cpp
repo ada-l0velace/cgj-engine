c8e4cf56f9cb0c9b5221f6257a51700c7cae9461
lolstorm92 <ricardo.s.campos@tecnico.ulisboa.pt>
2020-01-31 08:51:45 +0000

    Added model

---
+++ src/CGJ/Utils/Random.cpp
@@ -0,0 +1,11 @@
+#include <CGJ/Utils/Random.h>
+RandomSingleton& RandomSingleton::get() {
+    static RandomSingleton r;
+    return r;
+}
+
+RandomSingleton::RandomSingleton() {
+    m_randomEngine.seed((unsigned int)std::time(nullptr));
+    for (int i = 0; i < 5; i++)
+        intInRange(i, i * 5);
+}
